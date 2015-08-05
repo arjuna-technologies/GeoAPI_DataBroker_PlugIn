94906519ff925102b2d218a66c19f5cadb82f2bd
Stuart Wheater <stuart.wheater@arjuna.com>
2015-08-05 13:28:26 +0100

    Updated DataBroker version plus tests

--- geoapi-shapefile/src/test/java/com/arjuna/dbplugins/tests/geoapi/dataflownodes/shapefileconverterdataprocessor/ConverterTest.java
+++ geoapi-shapefile/src/test/java/com/arjuna/dbplugins/tests/geoapi/dataflownodes/shapefileconverterdataprocessor/ConverterTest.java
@@ -6,21 +6,41 @@ package com.arjuna.dbplugins.tests.geoapi.dataflownodes.shapefileconverterdatapr
 
 import java.io.File;
 import java.util.Collections;
+import java.util.UUID;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import com.arjuna.databroker.data.DataProcessor;
+import com.arjuna.databroker.data.connector.ObservableDataProvider;
 import com.arjuna.databroker.data.connector.ObserverDataConsumer;
+import com.arjuna.databroker.data.core.DataFlowNodeLifeCycleControl;
 import com.arjuna.dbplugins.geoapi.dataflownodes.ShapeFileConverterDataProcessor;
+import com.arjuna.dbutils.testsupport.dataflownodes.dummy.DummyDataSink;
+import com.arjuna.dbutils.testsupport.dataflownodes.dummy.DummyDataSource;
+import com.arjuna.dbutils.testsupport.dataflownodes.lifecycle.TestJEEDataFlowNodeLifeCycleControl;
 
 public class ConverterTest
 {
     @Test
     public void simplestConversion()
     {
-        ShapeFileConverterDataProcessor shapeFileDataProcessor = new ShapeFileConverterDataProcessor("ShapeFile Converter Data Processor", Collections.<String, String>emptyMap());
-        ObserverDataConsumer<File>      dataConsumer           = (ObserverDataConsumer<File>) shapeFileDataProcessor.getDataConsumer(File.class);
+        DataFlowNodeLifeCycleControl dataFlowNodeLifeCycleControl = new TestJEEDataFlowNodeLifeCycleControl();
+
+        DummyDataSource dummyDataSource        = new DummyDataSource("Dummy Data Source", Collections.<String, String>emptyMap());
+        DataProcessor   shapeFileDataProcessor = new ShapeFileConverterDataProcessor("ShapeFile Converter Data Processor", Collections.<String, String>emptyMap());
+        DummyDataSink   dummyDataSink          = new DummyDataSink("Dummy Data Sink", Collections.<String, String>emptyMap());
+
+        dataFlowNodeLifeCycleControl.completeCreationAndActivateDataFlowNode(UUID.randomUUID().toString(), dummyDataSource, null);
+        dataFlowNodeLifeCycleControl.completeCreationAndActivateDataFlowNode(UUID.randomUUID().toString(), shapeFileDataProcessor, null);
+        dataFlowNodeLifeCycleControl.completeCreationAndActivateDataFlowNode(UUID.randomUUID().toString(), dummyDataSink, null);
+
+        ((ObservableDataProvider<File>) dummyDataSource.getDataProvider(File.class)).addDataConsumer((ObserverDataConsumer<File>) shapeFileDataProcessor.getDataConsumer(File.class));
+        ((ObservableDataProvider<String>) shapeFileDataProcessor.getDataProvider(String.class)).addDataConsumer((ObserverDataConsumer<String>) dummyDataSink.getDataConsumer(String.class));
 
         File testFile = new File("/tmp/Gully_point/Gully_point.shp");
+        dummyDataSource.sendData(testFile);
 
-        dataConsumer.consume(null, testFile);
+        dataFlowNodeLifeCycleControl.removeDataFlowNode(dummyDataSource);
+        dataFlowNodeLifeCycleControl.removeDataFlowNode(shapeFileDataProcessor);
+        dataFlowNodeLifeCycleControl.removeDataFlowNode(dummyDataSink);
+
     }
 }
